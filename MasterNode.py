164aab747160cfe778a956bca6e8a3fa2ade51d7
Taher <taherbdossaji1102@gmail.com>
2022-11-25 23:16:54 +0530

    comments

--- MasterNode.py
+++ MasterNode.py
@@ -4,3 +4,9 @@ def read_operation():
 
 def write_operation():
     pass
+
+
+# User passes the input file to the client program to be stored in the cluster.
+# The client contacts the master node to schedule the WRITE operation.
+# The master node returns a list of worker nodes that the client has to write the data to.
+# Based on the list, the client program splits the input file into equally sized partitions and contacts the workers to store their respective partitions to the workers storage. After the client has successfully written the data to all the workers, client informs the user that the WRITE operation is successful.
