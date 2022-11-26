f9e55f71dfff9486e3908df2440bdca2c7c7145d
Utkarsh Bagaria <utkarshbagaria3008@gmail.com>
2022-11-26 22:11:28 +0530

    client socket

--- Client.py
+++ Client.py
@@ -1,6 +1,26 @@
 #client func
 import sys
 import MasterNode as mn
+import socket
+import threading
+
+def send_partition(address, content):
+    # create a socket object
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # get local machine name
+    host = address[0]
+    port = address[1]
+    # connection to hostname on the port.
+    s.connect((host, port))
+    # print(content)
+    # Receive no more than 1024 bytes
+    # msg = s.recv(1024)
+    # s.close()
+    # print(msg.decode('ascii'))
+    
+    s.sendall(content.encode('ascii'))
+    s.close()
+    return
 
 def writeop(filename):
     with open(filename, 'r') as file:
@@ -15,7 +35,13 @@ def writeop(filename):
             j+=1
         nodes_available = mn.nodesdict(n, filename)
         print(content)
-
+        for i in range(len(nodes_available.keys())):
+            print("Sending data to node: ", nodes_available[i])
+            # sending partition to the address in nodes_available with the partition name
+            send_thread=threading.Thread(target=send_partition,args=(nodes_available[i],k[i]))
+            send_thread.start()
+            # send_partition(nodes_available[i], content)
+            
 #reading a file filename given as command line argument
 print("FOR WRITE OPERATION: 1\n FOR READ OPERATION: 2\n FOR MR OPERATION: 3")
 choice = int(input("Enter your choice: "))
