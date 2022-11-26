3b27a0a9cc75f08564dedf64bc7f7d3ff6a1501b
tanvi0904 <tanvi.rajesh09@gmail.com>
2022-11-27 01:18:31 +0530

    MasterNodeup

--- MasterNode.py
+++ MasterNode.py
@@ -1,13 +1,15 @@
 import threading
-import WorkerNode as w
 import json
+import WorkerNode as w
 def nodesdictwrite(n=2,filename="xyz"):
     a=dict()
     filename, file_extension = filename.split(".")
     for i in range(n):
         a[i]=["127.0.0.1",23333+i,filename+str(i)+"."+file_extension]
-        connec=threading.Thread(target=w.conne,args=(a[i][0],a[i][1],a[i][2]))
+        connec=threading.Thread(target=w.conne,args=(a[i][0],a[i][1],a[i][2],'w'))
         connec.start()
+    fw=open('metadata.txt','a')
+    fw.write(filename+" "+json.dumps(a))
     return a
 
 def nodesdictread(n=2,filename="xyz"):
