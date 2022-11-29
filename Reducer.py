94376aec0aa024e84c18b5a763f55c9a53193b3f
Krishna Bajaj <bajaj.krishna01062002@gmail.com>
2022-11-29 19:21:37 +0530

    reducer added

--- Reducer.py
+++ Reducer.py
@@ -1,1 +1,19 @@
-pass
+import sys
+
+# query_word = sys.argv[1]
+word=""
+count = 1
+for line in sys.stdin:
+    line = line.strip()
+    current_word, c = line.rsplit(",", 1)
+    if word !=current_word:
+        if(word !=""):
+            print(f"{word} {count}")
+            count=1
+            word=current_word
+        else:
+            count=1
+            word=current_word
+    else:
+        count=count+1
+print(f"{word} {count}")
